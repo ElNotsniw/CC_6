124e0af9a5eedd3a9dbfcced1d96e06d449d00c6
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-06 17:05:13 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -32,3 +32,37 @@ const calculateSalesTax = function(amount, taxRate) {
 // Creating values to use for the function
 
 console.log(`10% Sales Tax of $500 is: $${calculateSalesTax(500, 0.10)}`);       // Output: 10% Sales Tax of $500 is $50
+
+
+
+
+// Task 3 - Arrow Function
+
+// Setting up the arrow function to determine bonus
+
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage
+
+    switch (performanceRating) {
+        case "Excellent":
+            bonusPercentage = 0.20;
+            break;
+        case "Good":
+            bonusPercentage = 0.10;
+            break;
+        case "Average":
+            bonusPercentage = 0.05
+            break;
+        default:
+            bonusPercentage = 0
+            break;
+    }
+
+    return salary * bonusPercentage;
+};
+
+// Setting up values to use with our arrow function to determine the bonus amount
+
+const salary = 70000;
+const performanceRating = "Excellent";
+console.log(`Bonus increase after getting an Excellent rating: $${calculateBonus(salary, performanceRating)}`);     // Output: Bonus increase after getting an Excellent rating: $14000
